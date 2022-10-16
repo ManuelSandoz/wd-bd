06c1d6ceb1644f780bd5bf0331181b87c0854dc2
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 <manuelsandoz28@gmail.com>
2022-10-16 11:24:36 -0400

    Added validation for email and localStorage for the inputs

--- lab-m2-lab2/javascript/review-page.functions.js
+++ lab-m2-lab2/javascript/review-page.functions.js
@@ -7,6 +7,11 @@
 
 "use strict";
 
+//THINGS TO DO FOR LAB
+// Javascript Form Validation (Input Type Validation Functions)
+// Javascript RegExp (Regular Expressions)
+// HTML Web Storage API (LocalStorage)
+
 // Code to activate the tooltips
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
@@ -15,4 +20,38 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 var loadImg = function (event) {
   var image = document.getElementById('f-review-img');
   image.srcset = URL.createObjectURL(event.target.files[0]);
+}
+
+// This funciton saves the form data in local storage
+function saveChanges() {
+  //This line retreives all the inputs from the form in an array form.
+  const inputs = document.querySelectorAll('#coffee-review  textarea, select option:checked, input[name="f-size-option"]:checked, input[type="text"], input[type="email"], input[type="datetime-local"], input[type="file"], input[type="checkbox"]:checked');
+
+  // Taking each element in the input array and saving them in the local storage.
+  //This uses the elements id as the key value.
+  try {
+    inputs.forEach(input => { // < possibly validate to prevent saving empty data
+      localStorage.setItem(input.id, input.value);
+    });
+
+    alert('Data saved sucessfully')
+
+  } catch (e) {
+    alert('There was an error saving the data. \n Please try again')
+  }
+}
+
+// This function will use regex to validate the email input
+function validateEmail() {
+  var regex = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/
+  var email = document.getElementById('f-email');
+
+  return regex.test(email.value);
+}
+
+// This function will validate through the inputs on the form
+function validateForm() {
+  if (!validateEmail()) {
+    alert('Please enter a valid email address')
+  }
 }
