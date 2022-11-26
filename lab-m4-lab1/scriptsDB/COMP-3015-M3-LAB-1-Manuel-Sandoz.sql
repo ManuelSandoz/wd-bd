8010a6cb204d3ebc9ba0cec37d6162a3dab22027
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 <manuelsandoz28@gmail.com>
2022-11-26 09:55:42 -0400

    Fixed queries and database structure

--- lab-m4-lab1/scriptsDB/COMP-3015-M3-LAB-1-Manuel-Sandoz.sql
+++ lab-m4-lab1/scriptsDB/COMP-3015-M3-LAB-1-Manuel-Sandoz.sql
@@ -5,23 +5,30 @@
 // Description: This file holds all the SQL commands used to create the database for the project. It also contains some queries as examples of how the data can be manipulated 
 */
 
+
+/* TODO
+1.
+*/
+
 -- Create the brew_review database
 CREATE DATABASE brew_review;
 
 -- Creating the users table
 CREATE TABLE tbl_USERS (
-	id int PRIMARY KEY,
-    firstName varchar(25) NOT NULL,
-    lastName varchar(25) NOT NULL
-)
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    firstName VARCHAR(25) NOT NULL,
+    lastName VARCHAR(25) NOT NULL 
+);
 
 -- Creating the reviews table
 CREATE TABLE tbl_REVIEWS (
-	email varchar(40) NOT NULL PRIMARY KEY,
-    drink varchar(15) NOT NULL,
-    size int NOT NULL,
-    review varchar(255) NOT NULL,
-    user_id int,
+    review_id INT PRIMARY KEY AUTO_INCREMENT,
+	email VARCHAR(40) NOT NULL,
+    drink VARCHAR(15) NOT NULL,
+    size VARCHAR(6) NOT NULL,
+    review VARCHAR(255) NOT NULL,
+    agreement BOOLEAN NOT NULL,
+    user_id INT NOT NULL,
     FOREIGN KEY(user_id) REFERENCES tbl_USERS(id)
 );
 
@@ -34,28 +41,28 @@ ALTER TABLE tbl_REVIEWS
 DROP COLUMN agrmnt;
 
 -- Inserting values into the users table 
-INSERT INTO tbl_USERS (id, firstName, lastName)
+INSERT INTO tbl_USERS (firstName, lastName)
 VALUES
-(1, 'Manuel', 'Sandoz'),
-(2, 'Alexandra', 'Rosario'),
-(3, 'Noah', 'Almeda'),
-(4, 'Irma', 'Santiago'),
-(5, 'Maura', 'Sandoz');
+('Manuel', 'Sandoz'),
+('Alexandra', 'Rosario'),
+('Noah', 'Almeda'),
+('Irma', 'Santiago'),
+('Maura', 'Sandoz');
 
 -- Inserting values into the reviews table 
-INSERT INTO tbl_REVIEWS (review_id, email, drink, size, review, user_id)
+INSERT INTO tbl_REVIEWS (email, drink, size, review, agreement, user_id)
 VALUES
-(1, 'manuel@email.com', 'latte', '12 oz.', "The best latte I've ever had in my life!!", 1),
-(2, 'arosario@email.com', 'mocha', '12 oz.', "It was missing chocolate",2),
-(3, 'noalmeda@email.com', 'mocha', '8 oz.', "This is my favourite drink", 3),
-(4, 'istago@gmail.com', 'cappuccino', '8 oz.', "The barista burnt the coffee", 4),
-(5, 'msandoz@email.com', 'espresso', '8 oz.', "This barista is the best ever. I will definitely be back", 5),
-(6, 'msandoz@email.com', 'mocha', '16 oz.', "I love mocha's and this one did not dissapoint", 5),
-(7, 'manuel@email.com', 'latte', '16 oz', "The coffe tasted like water! I will order a smaller size next time", 1),
-(8, 'manuel@email.com', 'cappuccino', '8 oz.', "This was my first time trying this drink and I loved it.", 1),
-(9, 'istago@email.com', 'latte', '12 oz.', "The barista was very friendly and skilled.", 4),
-(10, 'noalmeda@email.com', 'latte', '16 oz.', "Good coffee", 3),
-(11, 'manuel27@gmail.com', 'latte', '12 oz.', "Pretty good coffee. Will visit agian", 1);
+('manuel@email.com', 'latte', '12 oz.', "The best latte I've ever had in my life!!", 1, 1),
+('arosario@email.com', 'mocha', '12 oz.', "It was missing chocolate", 1, 2),
+('noalmeda@email.com', 'mocha', '8 oz.', "This is my favourite drink", 1, 3),
+('istago@gmail.com', 'cappuccino', '8 oz.', "The barista burnt the coffee", 1, 4),
+('msandoz@email.com', 'espresso', '8 oz.', "This barista is the best ever. I will definitely be back", 1, 5),
+('msandoz@email.com', 'mocha', '16 oz.', "I love mocha's and this one did not dissapoint", 1, 5),
+('manuel@email.com', 'latte', '16 oz.', "The coffe tasted like water! I will order a smaller size next time", 1, 1),
+('manuel@email.com', 'cappuccino', '8 oz.', "This was my first time trying this drink and I loved it.", 1, 1),
+('istago@email.com', 'latte', '12 oz.', "The barista was very friendly and skilled.", 1, 4),
+('noalmeda@email.com', 'latte', '16 oz.', "Good coffee", 1, 3),
+('manuel27@gmail.com', 'latte', '12 oz.', "Pretty good coffee. Will visit agian", 1, 1);
 
 -- Updating the values for review table
 UPDATE tbl_REVIEWS
