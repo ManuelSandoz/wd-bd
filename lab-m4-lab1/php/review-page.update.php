9661dae264fee5cd5b1212dd8b76548e67bc5934
Manuel Sandoz <manuelsandoz28@gmail.com>
2022-12-02 09:07:53 -0400

    dummy code

--- lab-m4-lab1/php/review-page.update.php
+++ lab-m4-lab1/php/review-page.update.php
@@ -15,6 +15,8 @@
   $drink = $_POST['drink'];
   $drinkSize = $_POST['drinkSize'];
   $review = $_POST['review'];
+  $visitDate = $_POST['visitDate'];
+  $picture = $_POST['picture'];
   $agreement = $_POST['agreement'];
 
   // Establishing connection to the DB
@@ -24,8 +26,41 @@
     die('ERROR: Database connection failed. ' . $dataBase -> connect_error);
   }
   else { 
-    $query = 'UPDATE TBL 
-      WRITE TEH THING HERE';
+    // $query = 'UPDATE TBL 
+    //   WRITE TEH THING HERE';
+
+      $query = "UPDATE 
+            `tbl_REVIEWS`
+        SET 
+            firstName = $firstName,
+            lastName = $lastName,
+            email = $email,
+            drink = $drink,
+            size = $drinkSize,
+            review = $review,
+            visitDate = $visitDate,
+            picture = $picture,
+            agreement = $agreement
+        FROM 
+            `tbl_REVIEWS`
+            JOIN tbl_USERS ON tbl_USERS.id = tbl_REVIEWS.user_id
+        WHERE 
+            firstName = $firstName 
+        AND 
+            lastName = $lastName";
+
+// UPDATE
+// user AS u
+// INNER JOIN
+// country AS c ON c.code = u.country_code
+// INNER JOIN
+// login AS l ON l.email = l.email
+// SET
+// u.name = 'John',
+// u.country_name = c.name,
+// l.country_name = c.name
+// WHERE
+// u.id = 3
     
     if ($dataBase -> query($query)) {
       echo(true);
